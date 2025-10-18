a59017482b8cd8740707ba38acd30e42ad45b879
mrattcs <j.t.k.hashemi@gmail.com>
2025-10-18 03:47:32 +0200

    initial commit

---
+++ shop/src/main/java/com/hollowstone/shop/HollowtoneShopApplication.java
@@ -0,0 +1,13 @@
+package com.hollowstone.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HollowtoneShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HollowtoneShopApplication.class, args);
+	}
+
+}
---
+++ shop/src/test/java/com/hollowstone/shop/HollowtoneShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hollowstone.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HollowtoneShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
